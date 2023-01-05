e05de6039b49a821724fc44d9b35c18a0bffa87e
Elianc22 <chavezelian2000@gmail.com>
2023-01-05 12:07:04 +0100

    creating login page layout

---
+++ src/components/conteiner/login/headerLogin.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../../styles/loginstyles/_stylesHeader.scss';
+
+const Headerlogin = () => {
+  return (
+    <div className="container-header">
+      <p>Hello world</p>
+    </div>
+  );
+};
+
+export default Headerlogin;
---
+++ src/components/father.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Headerlogin from './conteiner/login/headerLogin';
+
+const Father = () => {
+  return (
+    <div>
+      <Headerlogin></Headerlogin>
+    </div>
+  );
+};
+
+export default Father;
