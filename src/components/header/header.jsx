346814d5303e0bb95abda954194249200c2e884f
Elianc22 <chavezelian2000@gmail.com>
2023-01-14 19:39:34 +0100

    fixing header errors

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import './_header.scss';
+import { isMobile } from 'react-device-detect';
 
 const Header = () => {
   return (
     <header className="header">
-      <img
-        className="header-img"
-        src="https://www.freepnglogos.com/uploads/rick-and-morty-png/rick-and-morty-portal-moon-mod-download-35.png"></img>
-      <a className="header-link">Sign up!</a>
+      <div className="header-img-container">
+        <img
+          className="header-img"
+          src="https://www.freepnglogos.com/uploads/rick-and-morty-png/rick-and-morty-portal-moon-mod-download-35.png"
+        />
+      </div>
+      <div>
+        {isMobile ? (
+          <a className="header-link"> Sign up!</a>
+        ) : (
+          <p>
+            Don`t have an account? <a className="header-link"> Sign up!</a>
+          </p>
+        )}
+      </div>
     </header>
   );
 };
