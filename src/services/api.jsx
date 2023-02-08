99938abcc6debb1c0bf0f767f3071c0007edd135
Elianc22 <chavezelian2000@gmail.com>
2023-02-08 10:09:19 +0100

    connecting to the API to use it in the register form

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -3,6 +3,7 @@ const API_URL = 'https://sessions.cyclic.app/api';
 const signUp = (endpoint, userName, userEmail, password) => {
   return fetch(`${API_URL}/${endpoint}`, {
     method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       fullname: userName,
       email: userEmail,
